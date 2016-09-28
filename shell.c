57fd02ce73a46ef19e5abc3a04a9dd0e69507314
roy <endvroy@gmail.com>
2016-09-28 14:56:43 -0700

    completed parse_command

--- shell.c
+++ shell.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 void parse_line(char **commands[], size_t *n, char *line) {
     if (*commands == NULL) {
@@ -33,6 +34,51 @@ void parse_line(char **commands[], size_t *n, char *line) {
     }
 }
 
+void parse_command(char **argv[], size_t *n, char *line) {
+    if (*argv == NULL) {
+        *n = 10;
+        *argv = malloc((*n) * sizeof(char *));
+    }
+
+    char *remaining = line;
+    char *arg;
+    size_t i = 0;
+    while (1) {
+        arg = strsep(&remaining, " \n");
+
+        if (remaining == NULL) {    // end of line
+            if (*arg == '\0' || isspace(*arg)) {
+                if (i >= *n) {  // not enough size
+                    *n = i + 1;
+                    *argv = realloc(*argv, (*n) * sizeof(char *));
+                }
+                (*argv)[i + 1] = NULL; // append a NULL ptr
+                return;
+            }
+            if (i >= *n) {  // not enough size
+                *n = i + 2;
+                *argv = realloc(*argv, (*n) * sizeof(char *));
+            }
+            (*argv)[i] = arg;
+            (*argv)[i + 1] = NULL; // append a NULL ptr
+            return;
+        }
+        else {
+            if (*arg == '\0' || isspace(*arg)) {
+                continue;
+            }
+            else {
+                if (i >= *n) {  // not enough size
+                    *n = 2 * (*n);
+                    *argv = realloc(*argv, (*n) * sizeof(char *));
+                }
+                (*argv)[i] = arg;
+                i++;
+            }
+        }
+    }
+}
+
 int main(void) {
     char *line = NULL;
     size_t size = 0;
@@ -49,6 +95,22 @@ int main(void) {
         }
         else {
             printf("%s\n", commands[i]);
+            i++;
+        }
+    }
+
+    printf("--------------------------------\n");
+
+    char **argv = NULL;
+    size_t size1 = 0;
+    parse_command(&argv, &size1, commands[1]);
+    i = 0;
+    while (1) {
+        if (argv[i] == NULL) {
+            break;
+        }
+        else {
+            printf("%s\n", argv[i]);
             i++;
         }
     }
